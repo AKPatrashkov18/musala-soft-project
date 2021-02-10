fcee3579c13b41c58eb42c3dbd813d61bc5a0f30
SATodorov18 <59693603+SATodorov18@users.noreply.github.com>
2021-02-10 18:04:52 +0200

    Create mainMenu function

--- TeamGenerator/ConsoleApplication1/ConsoleApplication1.cpp
+++ TeamGenerator/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,7 +2,44 @@
 
 using namespace std;
 
+bool mainMenu()
+{
+    cout << "1. Add students or teachers"<<endl;
+    cout << "2. Generate team"<<endl;
+    cout << "3. Edit teams, students or teachers"<<endl;
+    cout << "4. Print teams, students or teachers" << endl;
+    cout << "9. Exit"<<endl;
+    int option;
+    cin >> option;
+    switch (option)
+    {
+        case 1:
+            return true;
+            break;
+        case 2:
+            return true;
+            break;
+        case 3:
+            return true;
+            break;
+        case 4:
+            return true;
+            break;
+        case 9:
+            return false;
+            break;
+        default:
+            return mainMenu();
+
+    }
+
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    bool exit=false;
+    do {
+        exit = mainMenu();
+    } while (exit);
+    return 0;
 }
